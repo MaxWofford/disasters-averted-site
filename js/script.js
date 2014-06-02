b83565b164781c54c7459062e286ee6aea999640
MaxWofford <max.speed.wofford@gmail.com>
2014-06-02 09:55:39 -0700

    Combined eventListeners

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
-//First check if our variable exists
-if (localStorage.AvertedCounter === 'undefined') {
-  console.log ("you dun fudged up")
-  localStorage.AvertedCounter = 0; //If it doesn't exist, then create it
-};
 
+//First check if our variable exists
+
+var setupVariable = function() {
+  if (typeof(localStorage.AvertedCounter) != "number" || typeof(localStorage.AvertedCounter) != "string") {
+    console.log ("you dun fudged up")
+    localStorage.AvertedCounter = 0; //If it doesn't exist, then create it
+  };
+  document.getElementById("counter").value = localStorage.AvertedCounter;
+}
 
 
 var up = function() {
+  setupVariable();
   localStorage.AvertedCounter++; //Add 1 to our counter
   document.getElementById("counter").value = localStorage.AvertedCounter; //Update the input to our new number
 }
 
 var down = function() {
+  setupVariable();
   localStorage.AvertedCounter--;
   document.getElementById("counter").value = localStorage.AvertedCounter;
 }
@@ -23,14 +29,12 @@ document.addEventListener('DOMContentLoaded', function() {
         up();
         console.log ("You get up cake!"); //A rewarding debug message
     });
-});
 
-
-document.addEventListener('DOMContentLoaded', function() {
     var iddown = document.getElementById('down');
     // onClick's logic below:
     iddown.addEventListener('click', function() {
         down();
         console.log ("You get down cake!"); //Another rewarding debug message
     });
+    setupVariable();
 });
