a2a755208664b61f4e8ef634fb0c20c3149f6432
MaxWofford <max.speed.wofford@gmail.com>
2014-06-02 10:06:21 -0700

    Resized and minimalized popup

--- js/script.js
+++ js/script.js
@@ -2,24 +2,17 @@
 //First check if our variable exists
 
 var setupVariable = function() {
-  if (typeof(localStorage.AvertedCounter) != "number" || typeof(localStorage.AvertedCounter) != "string") {
+  if (!localStorage.AvertedCounter) {
     console.log ("you dun fudged up")
     localStorage.AvertedCounter = 0; //If it doesn't exist, then create it
   };
-  document.getElementById("counter").value = localStorage.AvertedCounter;
+  document.getElementById("up").value = localStorage.AvertedCounter;
 }
-
 
 var up = function() {
   setupVariable();
   localStorage.AvertedCounter++; //Add 1 to our counter
-  document.getElementById("counter").value = localStorage.AvertedCounter; //Update the input to our new number
-}
-
-var down = function() {
-  setupVariable();
-  localStorage.AvertedCounter--;
-  document.getElementById("counter").value = localStorage.AvertedCounter;
+  document.getElementById("up").value = localStorage.AvertedCounter; //Update the input to our new number
 }
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -30,11 +23,5 @@ document.addEventListener('DOMContentLoaded', function() {
         console.log ("You get up cake!"); //A rewarding debug message
     });
 
-    var iddown = document.getElementById('down');
-    // onClick's logic below:
-    iddown.addEventListener('click', function() {
-        down();
-        console.log ("You get down cake!"); //Another rewarding debug message
-    });
     setupVariable();
 });
